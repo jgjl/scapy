1a860ca9703b458e67d93592270aee0f77bbec49
jgjl <git@jgjl.de>
2019-09-15 21:04:43 -0700

    Improve test: use two flags.

--- test/simple_to_list_test.py
+++ test/simple_to_list_test.py
@@ -3,7 +3,7 @@ from scapy.all import *
 
 pkt = (
         Ether(src="0:1:2:3:4:5", dst="5:4:3:2:1:0") /
-        IP(src="1.2.3.4", dst="4.3.2.1", flags="DF") /
+        IP(src="1.2.3.4", dst="4.3.2.1", flags=["DF", "MF"]) /
         UDP(sport=1234, dport=4321, chksum=True)
 )
 pkt.show()
